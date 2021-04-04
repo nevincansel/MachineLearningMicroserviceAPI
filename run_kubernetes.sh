40c36c4cdb1b619824b49f6871b88327611dbf59
nevin.cansel <ncansele@gmail.com>
2021-04-04 11:40:14 +0300

    fixed some errors and added circleci integration file.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,16 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
-export dockerpath=udacityproject3
+dockerpath=nevincansel/udacityproject3
 
 # Step 2
-kubectl run --image=$dockerpath:latest --port=80 udacityproject3
-
+# Run the Docker Hub container with kubernetes
+docker login
+kubectl run udacityproject3\
+    --image=$dockerpath\
+    --port=80 --labels app=udacityproject3
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 
+echo "Sleeping for 10 seconds while waiting for pod to come up."
+sleep 10
+
 # Step 4:
+# Forward the container port to a host
 kubectl port-forward udacityproject3 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=nevincansel/udacityproject3
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag udacityproject3:latest nevincansel/udacityproject3:latest
 
 # Step 3:
-# Push image to a docker repository
+docker push nevincansel/udacityproject3:latest
